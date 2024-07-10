0be82831a069ff17d1144cbb1e479d6d3d4eccf5
Pedro Zardetti <pedrozardetti1@gmail.com>
2024-07-10 19:34:29 -0300

    feat: ajuste posicionamento dos emails dos convidados

--- src/app.tsx
+++ src/app.tsx
@@ -1,4 +1,4 @@
-import { MapPin, Calendar, ArrowRight, UserRoundPlus, Settings2} from 'lucide-react'
+import { MapPin, Calendar, ArrowRight, UserRoundPlus, Settings2, X} from 'lucide-react'
 import { useState } from 'react';
 
 export function App() {
@@ -15,6 +15,10 @@ export function App() {
 
   function openGuestsModal() {
     setIsGuestsModalOpen(true)
+  }
+
+  function closeGuestsModal() {
+    setIsGuestsModalOpen(false)
   }
 
   return (
@@ -80,8 +84,54 @@ export function App() {
       
       {isGuestsModalOpen && (
         <div className="fixed inset-0 bg-black/60 flex items-center justify-center">
-            <div className='w-[640px] rounded-xl py-5 px-6 shadow-shape bg-zinc-900'>
-              <h2>Selecionar convidados</h2>
+            <div className='w-[640px] rounded-xl py-5 px-6 shadow-shape bg-zinc-900 space-y-5'>
+                <div className='space-y-2'>
+                <div className='flex items-center justify-between'>
+                <h2 className='text-lg font font-semibold'>Selecionar convidados</h2>
+                <button type='button' onClick={closeGuestsModal}>
+                  <X className='size-5 text-zinc-400'/>
+                </button>  
+              </div>
+              <p className='text-sm text-zinc-400'>
+                Os convidados irão receber e-mails para confirmar a participação na viagem.                
+              </p>
+                </div>
+
+                <div className='flex flex-wrap gap-2'>
+                  <div className='py-1.5 px-2.5 rounded-md bg-zinc-800 flex items-center gap-2'>
+                    <span className='text-zinc-300'>jessica.white44@yahoo.com</span>
+                    <button type='button'>
+                      <X className='size-4 text-zinc-400'/>
+                    </button>
+                  </div>
+
+                  <div className='py-1.5 px-2.5 rounded-md bg-zinc-800 flex items-center gap-2'>
+                    <span className='text-zinc-300'>jessica.white44@yahoo.com</span>
+                    <button type='button'>
+                      <X className='size-4 text-zinc-400'/>
+                    </button>
+                  </div>
+
+                  <div className='py-1.5 px-2.5 rounded-md bg-zinc-800 flex items-center gap-2'>
+                    <span className='text-zinc-300'>jessica.white44@yahoo.com</span>
+                    <button type='button'>
+                      <X className='size-4 text-zinc-400'/>
+                    </button>
+                  </div>
+
+                  <div className='py-1.5 px-2.5 rounded-md bg-zinc-800 flex items-center gap-2'>
+                    <span className='text-zinc-300'>jessica.white44@yahoo.com</span>
+                    <button type='button'>
+                      <X className='size-4 text-zinc-400'/>
+                    </button>
+                  </div>
+                </div>
+                <div className='w-full h-px bg-zinc-800'/>
+
+                <form className="p-2.5 bg-zinc-950 border border-zinc-800 rounded-lg">
+                  
+                </form>
+          
             </div>
         </div>
         )}
