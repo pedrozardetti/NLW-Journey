580088408c8fd525bdc8b1b6e65bb249a0205348
Pedro Zardetti <pedrozardetti1@gmail.com>
2024-07-11 19:03:30 -0300

    add route path para o login

--- src/app.tsx
+++ src/app.tsx
@@ -1,11 +1,15 @@
-import { Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { Login } from "./login";
 import { Home } from "./home";
 
 export function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Home/>} />
-    </Routes>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        
+      </Routes>
+    </Router>
   );
 }
