3789ab89cac9355b36477efc78742e207ffaecf1
Pedro Zardetti <pedrozardetti1@gmail.com>
2024-07-11 18:54:00 -0300

    add route

--- src/app.tsx
+++ src/app.tsx
@@ -1,31 +1,11 @@
+import { Route, Routes } from "react-router-dom";
+import { Login } from "./login";
+import { Home } from "./home";
+
 export function App() {
   return (
-    <div className="h-screen flex items-center justify-center bg-pattern bg-no-repeat bg-center">
-      <div className="max-w-3xl w-full px-6 text-center space-y-10">
-        <div className="flex flex-col items-center gap-3">
-          <img src="/logo.svg" alt="plann.er" />
-          <p className="text-zinc-300 text-lg">
-            Convide seus amigos e planeje sua próxima viagem
-          </p>
-          <p className="text-zinc-300 text-lg">
-            Antes, faça <span className="text-lime-300">login</span> ou{" "}
-            <span className="text-lime-300">cadastre-se!</span>
-          </p>
-
-          <p className="text-sm text-zinc-500 mt-16">
-            Ao planejar sua viagem pela plann.er você automaticamente concorda{" "}
-            <br />
-            com nossos{" "}
-            <a className="text-zinc-300 underline" href="#">
-              termos de uso{" "}
-            </a>{" "}
-            e{" "}
-            <a className="text-zinc-300 underline" href="#">
-              políticas de privacidade.
-            </a>
-          </p>
-        </div>
-      </div>
-    </div>
+    <Routes>
+      <Route path="/" element={<Home/>} />
+    </Routes>
   );
 }
