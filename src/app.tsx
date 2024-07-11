e03590c34171045dc5289f995bfa1f1130aaf001
Pedro Zardetti <pedrozardetti1@gmail.com>
2024-07-11 19:09:24 -0300

    add route para signup

--- src/app.tsx
+++ src/app.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { Login } from "./login";
 import { Home } from "./home";
+import { Signup } from "./sign-up";
 
 export function App() {
   return (
@@ -8,6 +9,8 @@ export function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+
         
       </Routes>
     </Router>
