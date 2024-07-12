f5f467e0a6718635017cefdf87e686ec291327c9
Pedro Zardetti <pedrozardetti1@gmail.com>
2024-07-12 17:48:59 -0300

    add create-trip route

--- src/app.tsx
+++ src/app.tsx
@@ -1,7 +1,8 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { Login } from "./login";
-import { Home } from "./home";
-import { Signup } from "./sign-up";
+import { Login } from "./components/login";
+import { Home } from "./components/home";
+import { Signup } from "./components/sign-up";
+import { CreateTrip } from "./components/create-trip";
 
 export function App() {
   return (
@@ -10,8 +11,7 @@ export function App() {
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
-
-        
+        <Route path="/create-trip" element={<CreateTrip />} />
       </Routes>
     </Router>
   );
