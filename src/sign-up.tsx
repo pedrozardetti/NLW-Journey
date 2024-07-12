c602f25aa3c5553354ff8e97feabe93daf83993b
Pedro Zardetti <pedrozardetti1@gmail.com>
2024-07-12 15:04:58 -0300

    feat: validação de email

--- src/sign-up.tsx
+++ src/sign-up.tsx
@@ -1,6 +1,22 @@
 import { Lock, Mail, UserRoundPlus } from "lucide-react";
+import { useEffect, useState } from "react";
+import classNames from "classnames"
 
 export function Signup() {
+  const [email, setEmail] = useState("");
+
+  const [emailFocus, setEmailFocus] = useState(false);
+
+  const [isEmailValid, setIsEmailValid] = useState(true);
+
+  useEffect(() => {
+    const validateEmail = /^([a-zA-Z0-9._%-]+@[a-zA-Z0-9.-]+.[a-zA-Z]{2,})$/;
+
+    if (emailFocus || email) {
+        setIsEmailValid(validateEmail.test(email));
+    }
+  }, [emailFocus, email]);
+  
   return (
     <div className="h-screen flex items-center justify-center">
       <div className="max-w-[485px] w-full max-h-[641px] h-full bg-zinc-800 rounded-lg flex flex-col pt-6 px-4 pb-14">
@@ -15,19 +31,32 @@ export function Signup() {
               placeholder="Seu nome completo"
             />
           </div>
-          <div className="flex gap-2 bg-zinc-950 w-full rounded-lg h-16 border border-zinc-700 items-center">
+          
+          <div className={classNames(
+            "flex gap-2 bg-zinc-950 w-full rounded-lg h-16 border items-center",
+            {
+                "border-zinc-700": isEmailValid,
+                "border-red-500": !isEmailValid
+            }
+          )}>
+            
             <Mail className="size-5 text-zinc-400 ml-4" />
             <input
+            
               className="bg-transparent outline-none w-full"
-              type="text"
+              type="e-mail"
               placeholder="E-mail"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              onFocus={() => setEmailFocus(true)}
+              onBlur={() => setEmailFocus(false)}
             />
           </div>
           <div className="flex gap-2 bg-zinc-950 w-full rounded-lg h-16 border border-zinc-700 items-center">
             <Lock className="size-5 text-zinc-400 ml-4" />
             <input
               className="bg-transparent outline-none w-full"
-              type="text"
+              type="password"
               placeholder="Senha"
             />
           </div>
