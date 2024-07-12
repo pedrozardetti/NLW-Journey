44f2242ad05745a1668a37861e37a619abadd55a
Pedro Zardetti <pedrozardetti1@gmail.com>
2024-07-12 18:51:36 -0300

    feat: add logica para mostrar quantidade de pessoas convidadas

--- src/components/create-trip.tsx
+++ src/components/create-trip.tsx
@@ -133,14 +133,15 @@ export function CreateTrip() {
                 className="flex items-center gap-2 flex-1 text-left"
               >
                 <UserRoundPlus className="size-5 text-zinc-400" />
-                <span className="text-zinc-400 text-lg flex-1">
-                  Quem estará na viagem?
-                </span>
-                <input
-                  type="text"
-                  placeholder=""
-                  className="bg-transparent text-lg placeholder-zinc-400 outline-none flex-1"
-                />
+                {emailsToInvite.length > 0 ? (
+                  <span className="text-zinc-100 text-lg flex-1">
+                    {emailsToInvite.length} pessoa(s) convidada(s)
+                  </span>
+                ) : (
+                  <span className="text-zinc-400 text-lg flex-1">
+                    Quem estará na viagem?
+                  </span>
+                )}
               </button>
 
               <div className="w-px h-6 bg-zinc-800" />
