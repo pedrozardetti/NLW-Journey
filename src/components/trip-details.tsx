be51ff9c163806a7ca7c2db9260bba9f36e9024f
Pedro Zardetti <pedrozardetti1@gmail.com>
2024-07-13 20:28:46 -0300

    feat: adicionar página TripDetailsPage com detalhes da viagem e botão para alterar local/data

--- src/components/trip-details.tsx
+++ src/components/trip-details.tsx
@@ -1,3 +1,25 @@
+import { Calendar, MapPin } from "lucide-react";
+
 export function TripDetailsPage() {
-    return <h1>Trip details</h1>
+  return (
+    <div className="max-w-6xl px-6 py-10 mx-auto">
+      <div className="px-4 h-16 rounded-xl bg-zinc-900 shadow-shape flex items-center justify-between">
+        <div>
+          <MapPin className="size-5 text-zinc-400" />
+          <span className="text-lg text-zinc-100">Florianópolis, Brasil</span>
+        </div>
+
+        <div>
+            <div className="flex items-center gap-2">
+          <Calendar className="size-5 text-zinc-400" />
+          <span className="text-lg text-zinc-100">17 a 23 de agosto
+          </span>
+          </div>
+
+            <div className="w-px h-6 bg-zinc-800"/>
+
+        </div>
+      </div>
+    </div>
+  );
 }
