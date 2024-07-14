d065ebf47c3a286f9229f10f852ab34116bd9c3d
Pedro Zardetti <pedrozardetti1@gmail.com>
2024-07-13 21:13:24 -0300

    feat: adicionar estrutura e mensagem de atividades para o dia 17 na página TripDetailsPage

--- src/components/trip-details.tsx
+++ src/components/trip-details.tsx
@@ -1,4 +1,4 @@
-import { Calendar, MapPin, Settings2 } from "lucide-react";
+import { Calendar, MapPin, Plus, Settings2 } from "lucide-react";
 
 export function TripDetailsPage() {
   return (
@@ -24,18 +24,30 @@ export function TripDetailsPage() {
         </div>
       </div>
 
-        <main className="flex gap-16">
-            <div className="flex-1">
-                <div className="flex items-center justify-between">
-                    <h2 className="text-3xl font-semibold">Atividades</h2>
+      <main className="flex gap-16">
+        <div className="flex-1 space-y-6">
+          <div className="flex items-center justify-between">
+            <h2 className="text-3xl font-semibold">Atividades</h2>
+            <button className="bg-lime-300 text-lime-950 rounded-lg px-5 py-2 font-medium flex items-center gap-2 hover:bg-lime-400">
+              <Plus className="size-5" />
+              Continuar
+            </button>
+          </div>
 
-                </div>
+          <div className="space-y-8">
+            <div className="space-y-2.5">
+              <div className="flex gap-2 items-baseline">
+                <span className="text-xl text-zinc-300 font-semibold">Dia 17</span>
+                <span className="text-xs text-zinc-500">Sábado</span>
+              </div>
+              <p className="text-zinc-500 text-sm">Nenhuma atividade cadastrada nessa data</p>
             </div>
-            <div className="w-80 ">
-                
-            </div>
-        </main>
+          </div>
+        </div>
 
+        <div className="w-80 "></div>
+
+      </main>
     </div>
   );
 }
