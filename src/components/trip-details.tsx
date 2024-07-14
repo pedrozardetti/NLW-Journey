573b92fd81029444498876716fb507aae18f1d99
Pedro Zardetti <pedrozardetti1@gmail.com>
2024-07-13 21:21:32 -0300

    feat: adicionar atividades para o dia 18 na página TripDetailsPage

--- src/components/trip-details.tsx
+++ src/components/trip-details.tsx
@@ -1,4 +1,4 @@
-import { Calendar, MapPin, Plus, Settings2 } from "lucide-react";
+import { Calendar, CircleCheck, MapPin, Plus, Settings2 } from "lucide-react";
 
 export function TripDetailsPage() {
   return (
@@ -24,7 +24,7 @@ export function TripDetailsPage() {
         </div>
       </div>
 
-      <main className="flex gap-16">
+      <main className="flex gap-16 px-4">
         <div className="flex-1 space-y-6">
           <div className="flex items-center justify-between">
             <h2 className="text-3xl font-semibold">Atividades</h2>
@@ -37,16 +37,42 @@ export function TripDetailsPage() {
           <div className="space-y-8">
             <div className="space-y-2.5">
               <div className="flex gap-2 items-baseline">
-                <span className="text-xl text-zinc-300 font-semibold">Dia 17</span>
+                <span className="text-xl text-zinc-300 font-semibold">
+                  Dia 17
+                </span>
                 <span className="text-xs text-zinc-500">Sábado</span>
               </div>
-              <p className="text-zinc-500 text-sm">Nenhuma atividade cadastrada nessa data</p>
+              <p className="text-zinc-500 text-sm">
+                Nenhuma atividade cadastrada nessa data
+              </p>
+            </div>
+
+            <div className="space-y-2.5">
+              <div className="flex gap-2 items-baseline">
+                <span className="text-xl text-zinc-300 font-semibold">
+                  Dia 18
+                </span>
+                <span className="text-xs text-zinc-500">Domingo</span>
+              </div>
+              <div className="space-y-2.5">
+                <div className="px-4 py-2.5 bg-zinc-900 rounded-xl shadow-shape flex items-center gap-3">
+                  <CircleCheck className="size-5 text-lime-300" />
+                  <span className="text-zinc-100">Academia em grupo</span>
+                  <span className="text-zinc-400 text-sm ml-auto">08:00h</span>
+                </div>
+              </div>
+              <div className="space-y-2.5">
+                <div className="px-4 py-2.5 bg-zinc-900 rounded-xl shadow-shape flex items-center gap-3">
+                  <CircleCheck className="size-5 text-lime-300" />
+                  <span className="text-zinc-100">Academia em grupo</span>
+                  <span className="text-zinc-400 text-sm ml-auto">08:00h</span>
+                </div>
+              </div>
             </div>
           </div>
         </div>
 
         <div className="w-80 "></div>
-
       </main>
     </div>
   );
