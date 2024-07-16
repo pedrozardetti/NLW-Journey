9c44063a08fc11dd264db55f4883e8a16be1afbc
Gabriel Dias <gabriel.diasramos@hotmail.com>
2024-07-16 02:43:27 -0300

    fix

--- src/app.tsx
+++ src/app.tsx
@@ -1,21 +1,24 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { Login } from "./components/login";
-import { Home } from "./components/home";
-import { Signup } from "./components/sign-up";
-import { CreateTrip } from "./components/create-trip";
-import { TripDetailsPage } from "./components/trip-details";
+import { Login } from "./pages/login/login";
+import { Home } from "./pages/home/home";
+import { Signup } from "./pages/signup/sign-up";
+import { CreateTrip } from "./pages/new-trip/create-trip";
+import { TripDetailsPage } from "./pages/trip/trip-details";
+import { AuthOwnerProvider } from "./hooks/owner";
 
 export function App() {
   return (
-    
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<Signup />} />
-        <Route path="/create-trip" element={<CreateTrip />} />
-        <Route path="/trips/:tripId" element={<TripDetailsPage />} />
-      </Routes>
-    </Router>
+    <AuthOwnerProvider>
+
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/create-trip" element={<CreateTrip />} />
+          <Route path="/trips/:tripId" element={<TripDetailsPage />} />
+        </Routes>
+      </Router>
+    </AuthOwnerProvider>
   );
 }
