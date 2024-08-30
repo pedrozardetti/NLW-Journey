e4cf89002a02da3a15ce22441a9e69370387e30d
Gabriel Dias <gabriel.diasramos@hotmail.com>
2024-08-30 02:01:54 -0300

    feat: cria metodos create, getbiyfilter, getactivities, delete

--- src/context/trip.tsx
+++ src/context/trip.tsx
@@ -1,35 +1,60 @@
+import { ReactNode } from "react";
 import { createContext, useCallback, useState } from "react";
 import { CreateTrip, Trip, TripList } from "../types/Trip";
 import api from "../service/api";
+import { ActivityRequest, ActivityResponse } from "../types/Activity";
+
+interface TripState {
+    Trip: Trip;
+}
 
 interface TripListState {
     TripList: TripList;
+}
+
+interface ActivityListState {
+    ActivityList: ActivityResponse[];
 }
 
 interface ResponseStatus {
     status: number;
 }
 
+interface ActivityResponseStatus {
+    status: number;
+}
+
 export interface TripContextData {
-    TripList: TripList;
     getTrips(page_number: number, page_size: number): Promise<void>;
     createTrip(trip: CreateTrip): Promise<void>;
+    getTripById(tripId: string): Promise<void>;
+    inviteParticipants(tripId: string, emails_to_invite: string[]): Promise<void>;
+    createActivity(tripId: string, activity: ActivityRequest): Promise<void>;
+    getActivitiesByFilter(tripId: string, filter: string): Promise<void>;
+    deleteActivityById(activityId: string): Promise<void>;
+    TripList: TripList;
     responseStatus: ResponseStatus;
+    tripData: TripState;
+    activityResponseStatus: ActivityResponseStatus;
+    activityList: ActivityListState;
 }
 
 
 const TripContext = createContext<TripContextData>({} as TripContextData);
 
-const TripProvider = ({ children }: any) => {
+const TripProvider = ({ children }: { children: ReactNode }) => {
 
     const [tripListData, setTripListData] = useState<TripListState>({} as TripListState);
 
+    const [tripData, setTripData] = useState<TripState>({} as TripState);
+
+    const [activityList, setActivityList] = useState<ActivityListState>({} as ActivityListState);
+
     const [responseStatus, setResponseStatus] = useState<ResponseStatus>({} as ResponseStatus);
 
+    const [activityResponseStatus, setActivityResponseStatus] = useState<ActivityResponseStatus>({} as ActivityResponseStatus);
 
     const token = sessionStorage.getItem("token") ?? null;
-
-    console.log(token);
 
     const createTrip = useCallback(async ({ destination, starts_at, ends_at, emails_to_invite }: CreateTrip) => {
         try {
@@ -60,20 +85,21 @@ const TripProvider = ({ children }: any) => {
 
             const tripData = response.data;
 
-            const trips: Trip[] = tripData.trips.map((trip: Trip) => ({
+            const trips: Trip[] = tripData.trips.map((trip: any) => ({
                 id: trip.id,
                 destination: trip.destination,
-                starts_at: new Date(trip.starts_at),
-                ends_at: new Date(trip.ends_at),
+                starts_at: trip.startsAt,
+                ends_at: trip.endsAt,
                 confirmed: trip.confirmed,
-                ownerId: trip.ownerId
+                ownerName: trip.ownerName,
+                ownerEmail: trip.ownerEmail
             }));
 
             const tripList: TripList = {
                 trips,
-                page_number: tripData.page_number,
-                page_size: tripData.page_size,
-                total_pages: tripData.total_pages
+                page_number: tripData.pageNumber,
+                page_size: tripData.pageSize,
+                total_pages: tripData.totalPages
             }
 
             setTripListData({ TripList: tripList });
@@ -82,14 +108,128 @@ const TripProvider = ({ children }: any) => {
         }
     }, [token]);
 
+    const getTripById = useCallback(async (tripId: string) => {
+        try {
+            const response = await api.get(`/trips/${tripId}`, {
+                headers: {
+                    Authorization: `Bearer ${token}`
+                }
+            });
+
+            const tripData = response.data;
+            const trip: Trip = {
+                id: tripData.id,
+                destination: tripData.destination,
+                starts_at: tripData.starts_at,
+                ends_at: tripData.ends_at,
+                confirmed: tripData.confirmed,
+                ownerId: tripData.ownerId
+            }
+
+            setTripData({ Trip: trip });
+        } catch (error) {
+            console.log(error);
+        }
 
 
+    }, [token]);
+
+    const inviteParticipants = useCallback(async (tripId: string, emails_to_invite: string[]) => {
+        try {
+            const response = await api.post(`/trips/invite/${tripId}`, {
+                emails_to_invite
+            }, {
+                headers: {
+                    Authorization: `Bearer ${token}`
+                }
+            });
+
+            const tripData = response.data;
+            const trip: Trip = {
+                id: tripData.id,
+                destination: tripData.destination,
+                starts_at: tripData.starts_at,
+                ends_at: tripData.ends_at,
+                confirmed: tripData.confirmed,
+                ownerId: tripData.ownerId
+            }
+
+            setTripData({ Trip: trip });
+        } catch (error) {
+            console.log(error);
+
+        }
+
+    }, [token])
+
+    const createActivity = useCallback(async (tripId: string, { name, occurs_at }: ActivityRequest) => {
+        try {
+            const response = await api.post(`/trips/${tripId}/activities`, {
+                name,
+                occurs_at
+            }, {
+                headers: {
+                    Authorization: `Bearer ${token}`
+                }
+            });
+
+            setActivityResponseStatus({ status: response.status });
+        } catch (error) {
+            console.log(error);
+        }
+    }, [token])
+
+    const getActivitiesByFilter = useCallback(async (tripId: string, filter: string) => {
+        try {
+            const response = await api.get(`/trips/${tripId}/activities?filter=${filter}`, {
+                headers: {
+                    Authorization: `Bearer ${token}`
+                }
+            });
+
+            const activityData = response.data;
+
+            const activityList: ActivityResponse[] = activityData.map((activity: ActivityResponse) => ({
+                id: activity.id,
+                name: activity.name,
+                occurs_at: new Date(activity.occurs_at)
+            }));
+
+            setActivityList({ ActivityList: activityList });
+        } catch (error) {
+            console.log(error);
+        }
+
+    }, [token])
+
+    const deleteActivityById = useCallback(async (activityId: string) => {
+        try {
+            const response = await api.delete(`/trips/${activityId}/activities`, {
+                headers: {
+                    Authorization: `Bearer ${token}`
+                }
+            });
+
+            setActivityResponseStatus({ status: response.status });
+        } catch (error) {
+            console.log(error);
+        }
+    }, [token])
 
     return (
         <TripContext.Provider value={{
             TripList: tripListData.TripList,
             getTrips: getTrips,
-            createTrip: createTrip
+            createTrip: createTrip,
+            responseStatus: responseStatus,
+            getTripById: getTripById,
+            inviteParticipants: inviteParticipants,
+            createActivity: createActivity,
+            activityResponseStatus: activityResponseStatus,
+            tripData: tripData,
+            activityList: activityList,
+            getActivitiesByFilter: getActivitiesByFilter,
+            deleteActivityById: deleteActivityById
         }}>
             {children}
         </TripContext.Provider>
