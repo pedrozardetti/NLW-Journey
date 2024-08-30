4401e716b6728aa3cd4cbae000b8316e13cba845
Gabriel Dias <gabriel.diasramos@hotmail.com>
2024-08-30 02:01:37 -0300

    fix

--- src/pages/logged-app/trip/trip-details.tsx
+++ src/pages/logged-app/trip/trip-details.tsx
@@ -12,8 +12,8 @@ import {
 export function TripDetailsPage() {
   return (
     <div className="max-w-6xl px-6 py-10 mx-auto space-y-8">
-      <div className="px-4 h-16 rounded-xl bg-zinc-900 shadow-shape flex items-center justify-between">
-        <div>
+      <div className="pl-6 pr-8 h-16 rounded-xl bg-zinc-900 shadow-shape flex items-center justify-between">
+        <div className="flex gap-2 items-center">
           <MapPin className="size-5 text-zinc-400" />
           <span className=" text-zinc-100">Florianópolis, Brasil</span>
         </div>
@@ -24,12 +24,6 @@ export function TripDetailsPage() {
             <span className=" text-zinc-100">17 a 23 de agosto</span>
           </div>
 
-          <div className="w-px h-6 bg-zinc-800" />
-
-          <button className="bg-zinc-800 text-zinc-200 rounded-lg px-5 py-2 font-medium flex items-center gap-2 hover:bg-zinc-700">
-            Alterar local/data
-            <Settings2 className="size-5" />
-          </button>
         </div>
       </div>
 
