c567c96dbdfb078b441f885f5e4bedc6238af547
Gabriel Dias <gabriel.diasramos@hotmail.com>
2024-08-29 22:56:19 -0300

    fix

--- src/types/Trip.ts
+++ src/types/Trip.ts
@@ -6,3 +6,17 @@ export interface Trip {
     confirmed: boolean,
     ownerId: string
 }
+
+export interface CreateTrip {
+    destination: string,
+    starts_at: Date,
+    ends_at: Date,
+    emails_to_invite: string[]
+}
+
+export interface TripList {
+    trips: Trip[];
+    page_number: number;
+    page_size: number;
+    total_pages: number;
+}
